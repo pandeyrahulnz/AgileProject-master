113a9f5dbde7927cab24016b203bd185cd4b902a
PANDEY RAHUL <pandeyrahulnz@gmail.com>
2020-10-06 22:57:07 +1300

    Commit

---
+++ AgileProject/Ingredients.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace AgileProject
+{
+    public class Ingredients
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+        public string RecipeName { get; set; }
+        public string imageurl { get; set; }
+        public string ingredients { get; set; }
+        public string Recipeurl { get; set; }
+    }
+}
